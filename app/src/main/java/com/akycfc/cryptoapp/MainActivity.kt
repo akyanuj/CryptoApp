3a5ae239ce5d569626b7cf4031f68055cba8cc01
AnujKumar <akyanuj.aky@gmail.com>
2024-02-17 02:34:21 +0530

    Git first push

---
+++ app/src/main/java/com/akycfc/cryptoapp/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.akycfc.cryptoapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import com.akycfc.cryptoapp.ui.theme.CryptoAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            CryptoAppTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+
+                }
+            }
+        }
+    }
+}
+
